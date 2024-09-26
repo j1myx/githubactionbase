1d222192e8cc640788464cee09f7ac20ec79611b
Jimy Navarro Cordova <jimynavarrocordova@gmail.com>
2024-09-26 13:46:02 -0500

    first commit

--- dist/index.js
+++ dist/index.js
@@ -29206,8 +29206,6 @@ function wrappy (fn, cb) {
 
 const httpClient = __nccwpck_require__(8922);
 
-const commits = github.payload.pull_request.commits;
-
 const validateCommitStandard = (commit) => {
     const regex = /^(ci|docs|feat|fix|perf|refactor|test|style|chore|revert)\([a-z]{2,20}\):( [A-Z]{4}-[0-9]{1,30})? [ a-zA-Z0-9áéíóú]*$/
 
@@ -29217,7 +29215,7 @@ const validateCommitStandard = (commit) => {
 /**
  * Validar la cantidad de commits.
  */
-const validateCommitQuantity = () => {
+const validateCommitQuantity = (commits) => {
     if (commits >= 8) {
         return 1;
     } else if (commits >= 6 && commits <= 7) {
@@ -29284,6 +29282,7 @@ const validatePRLinesQuantity = (totalChanges) => {
 
 const m1 = (github) => {
     const http = new httpClient.HttpClient();
+    const commits = github.payload.pull_request.commits;
 
     return new Promise((resolve, reject) => {
         http.get(github.payload.pull_request.commits_url).then(res => {
@@ -29306,7 +29305,7 @@ const m1 = (github) => {
                 const percentajeLines = changes / body.length;
 
                 setTimeout(() => {
-                    const m1_1 = validateCommitQuantity() * 0.2
+                    const m1_1 = validateCommitQuantity(commits) * 0.2
                     const m1_2 = validateCommitFilesQuantity(percentajeFiles) * 0.25
                     const m1_3 = validateCommitLinesQuantity(percentajeLines) * 0.25
                     const m1_4 = validatePRLinesQuantity(changes) * 0.3
@@ -29318,54 +29317,7 @@ const m1 = (github) => {
     })
 }
 
-
-/**
- * 
- * @returns {Promise<object>}
- */
-function calculateCommitPercentajes() {
-    const http = new httpClient.HttpClient()
-
-    return new Promise((resolve, reject) => {
-        http.get(github.payload.pull_request.commits_url).then(res => {
-            res.readBody().then(readBody => {
-                const body = JSON.parse(readBody)
-
-                let files = 0;
-                let changes = 0;
-
-                for (let i = 0; i < body.length; i++) {
-                    const commitUrl = body[i].url;
-
-                    http.get(commitUrl).then(resCommit => {
-                        files += resCommit.files;
-                        changes += resCommit.stats.total;
-                    });
-                }
-
-                const percentajeFiles = files / body.length;
-                const percentajeLines = changes / body.length;
-
-                setTimeout(() => {
-                    resolve({ files: percentajeFiles, changes: percentajeLines, totalChanges: changes })
-                }, 1000)
-            })
-        })
-    })
-}
-
-module.exports = new Promise((resolve, reject) => {
-    calculateCommitPercentajes().then(cp => {
-        const m1_1 = validateCommitQuantity(cp) * 0.2
-        const m1_2 = validateCommitFilesQuantity(cp) * 0.25
-        const m1_3 = validateCommitLinesQuantity(cp) * 0.25
-        const m1_4 = validatePRLinesQuantity(cp) * 0.3
-    
-        const total = m1_1 + m1_2 + m1_3 + m1_4
-
-        resolve({m1: total})
-    })
-})
+module.exports = { m1 }
 
 /***/ }),
 
