aaf1931efe1c758d56e99cce7811bbb50f86aef8
Jimy Navarro Cordova <jimynavarrocordova@gmail.com>
2024-09-27 06:58:55 -0500

    first commit

--- dist/index.js
+++ dist/index.js
@@ -29361,10 +29361,32 @@ module.exports = { m2 }
 /***/ }),
 
 /***/ 9276:
-/***/ ((module) => {
+/***/ ((module, __unused_webpack_exports, __nccwpck_require__) => {
+
+const github = __nccwpck_require__(1805);
+
+const pull_request = github.context.payload.pull_request;
+const modiffied = pull_request.additions + pull_request.deletions;
+
+const linesInPr = () => {
+    if (modiffied > 1200) {
+        return 1;
+    } else if (modiffied > 500 && modiffied <= 1200) {
+        return 2;
+    } else if (modiffied > 300 && modiffied <= 500) {
+        return 3;
+    } else if (modiffied > 50 && modiffied <= 300) {
+        return 4;
+    } else if (modiffied <=  50) {
+        return 5;
+    }
+}
 
 const m3 = () => {
-    return 5;
+    const m3_1 = 5 * 0.75; // cron job
+    const m3_2 = linesInPr() * 0.25;
+
+    return m3_1 + m3_2;
 }
 
 module.exports = { m3 }
--- rules/m3.js
+++ rules/m3.js
@@ -1,5 +1,27 @@
+const github = require('@actions/github');
+
+const pull_request = github.context.payload.pull_request;
+const modiffied = pull_request.additions + pull_request.deletions;
+
+const linesInPr = () => {
+    if (modiffied > 1200) {
+        return 1;
+    } else if (modiffied > 500 && modiffied <= 1200) {
+        return 2;
+    } else if (modiffied > 300 && modiffied <= 500) {
+        return 3;
+    } else if (modiffied > 50 && modiffied <= 300) {
+        return 4;
+    } else if (modiffied <=  50) {
+        return 5;
+    }
+}
+
 const m3 = () => {
-    return 5;
+    const m3_1 = 5 * 0.75; // cron job
+    const m3_2 = linesInPr() * 0.25;
+
+    return m3_1 + m3_2;
 }
 
 module.exports = { m3 }
