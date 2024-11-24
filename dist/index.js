ef68b02c79d67911fb28170ee6a9d476d878d217
Jimy Navarro Cordova <jimynavarrocordova@gmail.com>
2024-11-24 18:54:43 -0500

    show input

--- dist/index.js
+++ dist/index.js
@@ -31428,7 +31428,7 @@ const { m5 } = __nccwpck_require__(933)
 const { total } = __nccwpck_require__(2721)
 
 try {
-    console.log('Pull Request ID', core.getInput('pullRequestId'))
+    core.debug(core.getInput('pullRequestId'))
 
     const metricType = core.getInput('metric')
     let metricValue = null
